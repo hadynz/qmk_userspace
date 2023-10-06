58c0c113ef150871270dac40e1f54907fbf7cedc
Hady Osman <hadyos@gmail.com>
2023-10-07 01:35:00 +1300

    Simplify setup. Use qmk compile command

--- config.h
+++ config.h
@@ -1,16 +1,1 @@
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
-
-#include "../../config.h"
-
-// place overrides here
 #define TAPPING_TERM 300
-
-#define RGBW
-
-/* ws2812 RGB LED */
-#define RGB_DI_PIN B7
-#define RGBLED_NUM 30
-#define RGBLIGHT_ANIMATIONS
-
-#endif
