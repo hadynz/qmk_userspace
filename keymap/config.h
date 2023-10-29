6e98207e00a27d93b95f57204b0ebb9f1f280108
Hady Osman <hadyos@gmail.com>
2023-10-30 09:46:10 +1300

    More leader key experimentation

--- keymap/config.h
+++ keymap/config.h
@@ -1,1 +1,6 @@
 #define TAPPING_TERM 300
+
+// Per-key timeout (leader key configuration)
+// @ref: https://docs.qmk.fm/#/feature_leader_key
+#define LEADER_PER_KEY_TIMING
+#define LEADER_TIMEOUT 250
--- keymap/keymap.c
+++ keymap/keymap.c
@@ -83,25 +83,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 // Leader key configuration
-void leader_start_user(void) {
-    // Do something when the leader key is pressed
-}
-
 void leader_end_user(void) {
+  bool did_leader_succeed = false;
+
   if (leader_sequence_one_key(KC_F)) {
-      // Leader, f => Types the below string
       SEND_STRING("Leader F");
+      did_leader_succeed = true;
   if (leader_sequence_one_key(KC_D)) {
-      // Leader, f => Types the below string
       SEND_STRING("Leader D");
+      did_leader_succeed = true;
   } else if (leader_sequence_two_keys(KC_D, KC_D)) {
-      // Leader, d, d => Ctrl+A, Ctrl+C
       SEND_STRING(SS_LCTL("a") SS_LCTL("c"));
+      did_leader_succeed = true;
   } else if (leader_sequence_three_keys(KC_D, KC_D, KC_S)) {
-      // Leader, d, d, s => Types the below string
       SEND_STRING("https://start.duckduckgo.com\n");
+      did_leader_succeed = true;
   } else if (leader_sequence_two_keys(KC_A, KC_S)) {
-      // Leader, a, s => GUI+S
       tap_code16(LGUI(KC_S));
+      did_leader_succeed = true;
+  }
+
+  if (did_leader_succeed) {
+    SEND_STRING('s');
+  } else {
+    SEND_STRING('f');
   }
 }
