d140a8a0d8ead02098d5e64ed2374a286e2d2681
Hady Osman <hadyos@gmail.com>
2023-12-12 10:38:21 +1300

    Indentation changes

--- keymap_keychron_q2/keymap.c
+++ keymap_keychron_q2/keymap.c
@@ -1,19 +1,3 @@
-/* Copyright 2021 @ Keychron (https://www.keychron.com)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 
 // Tap Dance declarations
@@ -89,10 +73,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-  [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+  [MAC_BASE]     = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
   [COLEMAK_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-  [_FN1]     = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
-  [_FN2]     = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
-  [_FN3]     = { ENCODER_CCW_CW(_______, _______) },
+  [_FN1]         = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
+  [_FN2]         = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
+  [_FN3]         = { ENCODER_CCW_CW(_______, _______) },
 };
 #endif
