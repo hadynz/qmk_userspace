48ea6ab18240c68efc62b978dcebe66fb1719d61
Hady Osman <hadyos@gmail.com>
2025-02-22 16:57:09 +1300

    Drop conflicting combos. Rename alias keycodes

--- keymap_keychron_q2/keymap.c
+++ keymap_keychron_q2/keymap.c
@@ -14,14 +14,16 @@ tap_dance_action_t tap_dance_actions[] = {
   [TD_PLAY_NEXT] = ACTION_TAP_DANCE_DOUBLE(KC_MPLY, KC_MNXT), // Tap once for media play, twice for media next
 };
 
-// Custom Keycodes
+// Custom Keycode aliases
 #define MEH_TAB MEH_T(KC_TAB)
 #define HYP_ESC HYPR_T(KC_ESC)
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
-#define C_ZOOM1 TD(TD_F9_F10)
-#define C_ZOOM2 TD(TD_F11_F12)
-#define C_MEDIA TD(TD_PLAY_NEXT)
+#define C_ZOOM1 TD(TD_F9_F10) // Zoom control -- Tap once for F9, twice for F10
+#define C_ZOOM2 TD(TD_F11_F12) // Zoom control -- Tap once for F11, twice for F12
+#define C_MEDIA TD(TD_PLAY_NEXT) // Media control -- Tap once for media play, twice for media next
+#define LAYER_C MO(_FN2) // Layer switch -- Momentarily turn on _FN2 layer
+#define LAYER_F MO(_FN3) // Layer switch -- Momentarily turn on _FN3 layer
 
 // Layers declarations
 enum layers{
@@ -33,19 +35,19 @@ enum layers{
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [MAC_BASE] = LAYOUT_ansi_67(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,          C_MEDIA,
-    MEH_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,          C_ZOOM1,
-    HYP_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,           C_ZOOM2,
-    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT, KC_UP,
-    KC_LCTL, KC_LOPT, KC_LCMD,                            KC_SPC,                             KC_RCMD, MO(_FN3), KC_ROPT,  KC_LEFT, KC_DOWN, KC_RGHT
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          C_MEDIA,
+    MEH_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          C_ZOOM1,
+    HYP_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           C_ZOOM2,
+    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,
+    KC_LCTL, KC_LOPT, KC_LCMD,                            KC_SPC,                             KC_RCMD, LAYER_F, KC_ROPT, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [COLEMAK_BASE] = LAYOUT_ansi_67(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,          C_MEDIA,
-    MEH_TAB, KC_Q,    KC_W,    KC_F,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_O,    KC_SCLN, KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
-    HYP_ESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,            KC_ENT,           KC_HOME,
-    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT, KC_UP,
-    KC_LCTL, KC_LOPT, KC_LCMD,                            KC_SPC,                             KC_RCMD, MO(_FN2), MO(_FN3), KC_LEFT, KC_DOWN, KC_RGHT
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,          C_MEDIA,
+    MEH_TAB, KC_Q,    KC_W,    KC_F,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_O,    KC_SCLN, KC_LBRC,  KC_RBRC, KC_BSLS,          KC_DEL,
+    HYP_ESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,           KC_ENT,           KC_HOME,
+    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSFT, KC_UP,
+    KC_LCTL, KC_LOPT, KC_LCMD,                            KC_SPC,                             KC_RCMD, LAYER_C, LAYER_F, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_FN2] = LAYOUT_ansi_67(
@@ -71,10 +73,6 @@ const uint16_t PROGMEM cmd_q_combo[] = {KC_Q, KC_W, COMBO_END};
 const uint16_t PROGMEM cmd_w_combo[] = {KC_W, KC_E, COMBO_END};
 const uint16_t PROGMEM cmd_l_combo[] = {KC_L, KC_SCLN, COMBO_END};
 const uint16_t PROGMEM cmd_f_combo[] = {KC_F, KC_G, COMBO_END};
-const uint16_t PROGMEM cmd_k_combo[] = {KC_K, KC_L, COMBO_END};
-const uint16_t PROGMEM cmd_t_combo[] = {KC_T, KC_Y, COMBO_END};
-const uint16_t PROGMEM cmd_o_combo[] = {KC_O, KC_P, COMBO_END};
-const uint16_t PROGMEM cmd_n_combo[] = {KC_N, KC_M, COMBO_END};
 const uint16_t PROGMEM cmd_lbracket_combo[] = {KC_LBRC, KC_RBRC, COMBO_END};
 const uint16_t PROGMEM cmd_rbracket_combo[] = {KC_RBRC, KC_BSLS, COMBO_END};
 const uint16_t PROGMEM undo_combo_keys[] = {KC_Z, KC_X, COMBO_END};
@@ -89,10 +87,6 @@ combo_t key_combos[] = {
   COMBO(cmd_w_combo, LGUI(KC_W)),
   COMBO(cmd_l_combo, LGUI(KC_L)),
   COMBO(cmd_f_combo, LGUI(KC_F)),
-  COMBO(cmd_k_combo, LGUI(KC_K)),
-  COMBO(cmd_t_combo, LGUI(KC_T)),
-  COMBO(cmd_o_combo, LGUI(KC_O)),
-  COMBO(cmd_n_combo, LGUI(KC_N)),
   COMBO(cmd_lbracket_combo, LGUI(KC_LBRC)),
   COMBO(cmd_rbracket_combo, LGUI(KC_RBRC)),
   COMBO(undo_combo_keys, LGUI(KC_Z)),
